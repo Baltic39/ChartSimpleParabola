fc53b9e5bc21e722dcdb65a0d4b507ef194396cd
Dmitrii Efimov <user_name_2003@mail.ru>
2025-09-16 19:40:21 +0300

    first programm

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChartSimpleParabola
+{
+    public partial class Form1 : Form
+    {
+        private double h;
+        private int a, b;
+        private double x, y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a = -2;
+            b = 2;
+            h = 0.5;
+            x = a;
+
+            while (x <= b)
+            {
+                y = x * x;
+                this.chart1.Series[0].Points.AddXY(x, y);
+                x += h;
+            }
+
+            x = 1.2;
+            y = x * x;
+            this.chart1.Series[1].Points.AddXY(x, y);
+                
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                chart1.SaveImage(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                Bitmap bm = new Bitmap(ms);
+                Clipboard.SetImage(bm);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.SaveImage("Graphic_image", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png);
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ChartSimpleParabola")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChartSimpleParabola")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("30b0901f-0fc1-4463-8f06-dc39fe6e1b07")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
